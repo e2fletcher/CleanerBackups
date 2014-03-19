46bc1bf740d7fc0c9dfb07d3d7d4f72400ffc9f2
e2fletcher <e2fletcher@gmail.com>
2014-03-19 14:31:53 -0430

    first commit

---
+++ PKGBUILD
@@ -0,0 +1,12 @@
+# Maintainer: Ender Fletcher <e2fletcher@gmail.com>
+pkgname=CleanerBackups
+pkgver=0.1
+pkgrel=1
+pkgdesc="Limpiador de archivos para hacer copias de seguridad en Windows"
+arch=('i686')
+url="https://www.facebook.com/e2fletcher"
+license=('GPL3')
+
+package() {
+    cmake
+}
---
+++ cleanerb.cpp
@@ -0,0 +1,40 @@
+#include <ftw.h>
+#include <fnmatch.h>
+#include <cstdio>
+
+static const char *filters_executable[] = {
+    "*.scr", "*.exe", "*.pif", "*.bat", 
+    "*.reg", "*.dll", "*.vbs", "*.com",
+    "*.xls", "*.msi", "*.eml"
+};
+
+static const char *filters_executable_directory[] = {
+    "RECICLER"
+};
+
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+    /* if it's a file */
+    if (typeflag == FTW_F) {
+        int i;
+        /* for each filter, */
+        for (i = 0; i < sizeof(filters_executable) / sizeof(filters_executable[0]); i++) {
+            /* if the filename matches the filter, */
+            if (fnmatch(filters_executable[i], fpath, FNM_CASEFOLD) == 0) {
+                /* do something */
+                printf("Posible amenaza: %s\n", fpath);
+                break;
+            }
+        }
+    }
+
+    //if (typeflag == FTW_D)
+	//printf("\033[1;31m%s\033[0m\n", fpath);
+
+    /* tell ftw to continue */
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    ftw(argv[1], callback, 16);
+    return 0;
+}
